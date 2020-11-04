a730e5aa75be758a9d5810fc7aa47c65d781bebc
roma1712 <73949481+roma1712@users.noreply.github.com>
2020-11-05 00:20:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import constants
 
 bot = telebot.TeleBot("1424440257:AAGsf3PxldRiAvLizaw362pKSQ8b8UJAW7k")
 @bot.message_handler(content_types=['text','audio','video','voice','photo','sticker'])
+def get(message):
+	if message.text.lower() == "db0605":
+		bot.send_message(message.from_user.id, "Босс все в норме , вам кофе чай или водку?")
+		if message.text.lower() == "водку":
+			bot.send_message(message.from_user.id, "Отлично")
+		else:
+			bot.send_message(message.from_user.id, "Паршиво")
+	else:
+		bot.send_message(message.from_user.id, "Вы не босс!")
 def send_welcome(message):
 	if message.text.lower() == "я серый":
 		bot.send_message(message.from_user.id, "Мой господин!")
@@ -10,6 +19,9 @@ def send_welcome(message):
 		bot.send_message(message.from_user.id, "Мой господин!")
 	elif message.text.lower() == "я дб":
 		bot.send_message(message.from_user.id, "Пошел нахуй выродок!")
+	elif message.text.lower() == "/admin":
+		bot.send_message(message.from_user.id, "Введите пароль")
+		bot.register_next_stand_handler(message.from_user.id, get(/admin))
 	else:
 		bot.send_message(message.chat.id,constants.random_message())
 	if 'лох' in message.text.lower():
