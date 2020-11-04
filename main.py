1e55582fd28440a1a68347bdc2f70e482a84d01c
roma1712 <73949481+roma1712@users.noreply.github.com>
2020-11-05 01:59:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,10 @@ def send_welcome(message):
 		bot.send_message(message.from_user.id, "Мой господин!")
 	elif message.text.lower() == "я дб":
 		bot.send_message(message.from_user.id, "Пошел нахуй выродок!")
-	elif  message.text.lower() == "/boss":
-		bot.send_message(message.from_user.id, "Введи пароль")
-			
 			
 	else:
 		bot.send_message(message.chat.id,constants.random_message())
-	if 'лох' in message.text.lower():
-		bot.send_message(message.from_user.id, "Лошара твоя мамка!")
-	elif 'db0605' in message.text.lower():
+	if 'db0605' in message.text.lower():
 		bot.send_message(message.from_user.id, "Как вы босс?"
 
 	
