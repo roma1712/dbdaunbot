c1fb85462c5cecac7a6928a8f77b3437bb38f9fb
roma1712 <73949481+roma1712@users.noreply.github.com>
2020-11-04 20:21:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ bot = telebot.TeleBot("1424440257:AAGsf3PxldRiAvLizaw362pKSQ8b8UJAW7k")
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
 	bot.send_message(message.chat.id,constants.random_message())
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
 	if message.text.lower() == "привет":
 		bot.send_message(message.from_user.id, "И тебе привет!")
 	if 'бот' in message.text.lower():
