9b6748561ace57a8335bbe0cddfd800ed4797c8a
roma1712 <73949481+roma1712@users.noreply.github.com>
2020-11-04 21:33:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,21 @@ bot = telebot.TeleBot("1424440257:AAGsf3PxldRiAvLizaw362pKSQ8b8UJAW7k")
 def send_welcome(message):
 	if message.text.lower() == "я серый":
 		bot.send_message(message.from_user.id, "Мой господин!")
-	elif message.text.lower() == "я рома":
+	if message.text.lower() == "я рома":
 		bot.send_message(message.from_user.id, "Мой господин!")
-	elif message.text.lower() == "я дб":
+	if message.text.lower() == "я дб":
 		bot.send_message(message.from_user.id, "Пошел нахуй выродок!")
-	elif message.text.lower() == "admin":
+	if message.text.lower() == "admin":
 		bot.send_message(message.from_user.id, "Введите пароль")
-	if message.text.lower() == "db0605":
-		bot.send_message(message.from_user.id, "Босс,все под контролем!")
-		bot.send_message(message.from_user.id, "Что выберете?Кофе,чай или водку?")
-		if message.text.lower() == "водку":
-			bot.send_message(message.from_user.id, "Отлично босс!")
+		if message.text.lower() == "db0605":
+			bot.send_message(message.from_user.id, "Босс,все под контролем!")
+			bot.send_message(message.from_user.id, "Что выберете?Кофе,чай или водку?")
+			if message.text.lower() == "водку":
+				bot.send_message(message.from_user.id, "Отлично босс!")
+			else:
+				bot.send_message(message.from_user.id, "Паршивый выбор")
 		else:
-			bot.send_message(message.from_user.id, "Паршивый выбор")
-	else:
-		bot.send_message(message.from_user.id, "Иди нахуй ,ты не босс!")
-					
-			
-	
+			bot.send_message(message.from_user.id, "Иди нахуй ,ты не босс!")	
 	else:
 		bot.send_message(message.chat.id,constants.random_message())
 	if 'лох' in message.text.lower():
