d273f8db94de42aa82ed54984ac49dcf8a291964
roma1712 <73949481+roma1712@users.noreply.github.com>
2020-11-04 21:23:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import constants
 
 
 bot = telebot.TeleBot("1424440257:AAGsf3PxldRiAvLizaw362pKSQ8b8UJAW7k")
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text','audio','video','voice','photo','sticker'])
 def send_welcome(message):
 	if message.text.lower() == "я серый":
 		bot.send_message(message.from_user.id, "Мой господин!")
@@ -11,10 +11,24 @@ def send_welcome(message):
 		bot.send_message(message.from_user.id, "Мой господин!")
 	elif message.text.lower() == "я дб":
 		bot.send_message(message.from_user.id, "Пошел нахуй выродок!")
+	elif message.text.lower() == "/admin":
+		bot.send_message(message.from_user.id, "Введите пароль")
+		if message.text.lower() == "db0605":
+			bot.send_message(message.from_user.id, "Босс,все под контролем!")
+			bot.send_message(message.from_user.id, "Что выберете?Кофе,чай или водку?")
+				if message.text.lower() == "водку":
+					bot.send_message(message.from_user.id, "Отлично босс!")
+				else:
+					bot.send_message(message.from_user.id, "Паршивый выбор")
+		else:
+			bot.send_message(message.from_user.id, "Иди нахуй ,ты не босс!")
+					
+			
+	
 	else:
 		bot.send_message(message.chat.id,constants.random_message())
-	if 'дб' in message.text.lower():
-		bot.send_message(message.from_user.id, "Не смей произносить это поганное имя (ДБ)")
+	if 'лох' in message.text.lower():
+		bot.send_message(message.from_user.id, "Лошара твоя мамка!")
 	
 
 bot.polling(none_stop = True)
