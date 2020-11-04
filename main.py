e76a69f6e4dcf01730284f4858797acc35a80476
roma1712 <73949481+roma1712@users.noreply.github.com>
2020-11-04 19:46:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot("1424440257:AAGsf3PxldRiAvLizaw362pKSQ8b8UJAW7k")
 def send_welcome(message):
 	bot.send_message(message.chat.id,constants.random_message())
 def send_welcom(message):
-	if message.text.lower() == "Я Серый":
-		bot.send_message(message.from_user.id, "Да мой господин!")
+	if message.text == "Я Cерый":
+		bot.send_message(message.chat.id, "Да мой господин!")
 
 bot.polling(none_stop = True)
