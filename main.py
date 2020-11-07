ffb8a346c4e2931b2b8f58357ff507e4725eaa52
roma1712 <73949481+roma1712@users.noreply.github.com>
2020-11-07 23:14:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -147,15 +147,29 @@ def ques6(message):
 	quess5 = message.text.lower()
 	if quess5 == "черный":
 		score += 1
-		bot.send_message(message.chat.id, "Вы набрали", int(score), "очков!")
-		bot.register_next_step_handler(message, send_welcome)
-		achieve += "Тест Ромы пройден на", int(score), "очков"	
+		if score == 0
 	else:
 		score += 0
-		bot.send_message(message.chat.id, "Вы набрали", int(score), "очков!")
-		bot.register_next_step_handler(message, send_welcome)
-		achieve += "Тест Ромы пройден на", int(score), "очков"
-	
+
+if score == 0:
+	bot.send_message(message.chat.id, "Вы прошли на 0 очков.Ужасно!")
+	bot.register_next_step_handler(message, send_welcome)
+if score == 1:
+	bot.send_message(message.chat.id, "Вы прошли на 1 очков.Отвратительно!")
+	bot.register_next_step_handler(message, send_welcome)
+if score == 2:
+	bot.send_message(message.chat.id, "Вы прошли на 2 очков.Такое себе!")
+	bot.register_next_step_handler(message, send_welcome)
+if score == 3:
+	bot.send_message(message.chat.id, "Вы прошли на 3 очков.Нормально!")
+	bot.register_next_step_handler(message, send_welcome)
+if score == 4:
+	bot.send_message(message.chat.id, "Вы прошли на 0 очков.Отлично!")
+	bot.register_next_step_handler(message, send_welcome)
+if score == 5:
+	bot.send_message(message.chat.id, "Вы прошли на 0 очков.Я не знаю как тебя зовут,но уже люблю тебя!")
+	bot.register_next_step_handler(message, send_welcome)
+
 
 
 
